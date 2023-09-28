7412191ea18d031ae26a13b408def85ce87b289b
tsm <shimei820@gmail.com>
2023-09-28 09:01:02 +0800

    pass compling

--- modules/axhal/src/arch/aarch64/trap.rs
+++ modules/axhal/src/arch/aarch64/trap.rs
@@ -8,10 +8,7 @@ use hypercraft::lower_aarch64_synchronous;
 
 use super::TrapFrame;
 
-// #[cfg(not(feature = "hv"))]
 global_asm!(include_str!("trap.S"));
-#[cfg(feature = "hv")]
-global_asm!(include_str!("trap_hv.S"));
 
 #[repr(u8)]
 #[derive(Debug)]
